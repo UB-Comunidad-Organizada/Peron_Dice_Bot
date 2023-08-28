4116bc44267912b7c67142dd67266cbbdfe1be77
Gabriel Robles <44828370+roblesgabriel27@users.noreply.github.com>
2023-08-28 10:46:45 -0300

    Metemos las citas

--- Bot.py
+++ Bot.py
@@ -9,12 +9,31 @@ load_dotenv(dotenv_path=os.path.join(os.getcwd(), 'env', 'config.env'))
 # Accede al valor del TOKEN
 TOKEN = os.getenv('Token')
 
+# Lista de citas disponibles
+citas_disponibles = [
+    "Cita 1",
+    "Cita 2",
+    "Cita 3"
+]
+
 
 async def Hola(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hola {update.effective_user.first_name}')
 
+
+async def SolicitarCita(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Obtener el índice de la cita seleccionada
+    cita_index = int(context.args[0]) - 1
+
+    if cita_index < 0 or cita_index >= len(citas_disponibles):
+        await update.message.reply_text("La cita seleccionada no es válida.")
+    else:
+        cita_seleccionada = citas_disponibles[cita_index]
+        await update.message.reply_text(f"Has solicitado la cita: {cita_seleccionada}")
+
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("Hola", Hola))
+app.add_handler(CommandHandler("SolicitarCita", SolicitarCita))
 
 app.run_polling()
