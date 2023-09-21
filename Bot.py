d4c6e9233c3357e4fba2ae518331c57389c31047
Gabriel Robles <44828370+roblesgabriel27@users.noreply.github.com>
2023-09-20 22:22:57 -0300

    Maneja todos los mensajes

--- Bot.py
+++ Bot.py
@@ -2,7 +2,13 @@ import os
 import json
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    Filters,
+    MessageHandler
+)
 
 # Cargar variables de entorno desde el archivo config.env en la carpeta env
 load_dotenv(dotenv_path=os.path.join(os.getcwd(), 'env', 'config.env'))
@@ -29,9 +35,6 @@ async def Hola(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def Verdad(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if update.message is None:
-        return
-
     if len(context.args) == 0:
         await update.message.reply_text(
             "No se ha proporcionado un número de verdad."
@@ -48,9 +51,21 @@ async def Verdad(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     except IndexError:
         await update.message.reply_text("La verdad seleccionada no es válida.")
 
+
+async def handle_message(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    await update.message.reply_text("Lo siento, no entiendo ese comando.")
+
+
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("Hola", Hola))
 app.add_handler(CommandHandler("Verdad", Verdad))
 
+# Maneja todos los mensajes de texto
+app.add_handler(MessageHandler(Filters.text, handle_message))
+
+# arranca el Bot
 app.run_polling()
